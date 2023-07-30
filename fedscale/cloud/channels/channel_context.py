8a85ff48f2b23b6a3e83b4afd3208d22a68e2f38
EricDinging <ericdingg16@outlook.com>
2023-07-30 17:37:06 -0400

    Init client communication

--- fedscale/cloud/channels/channel_context.py
+++ fedscale/cloud/channels/channel_context.py
@@ -3,17 +3,31 @@ import logging
 import grpc
 
 import fedscale.cloud.channels.job_api_pb2_grpc as job_api_pb2_grpc
+from propius.channels import propius_pb2
+from propius.channels import propius_pb2_grpc
 
 MAX_MESSAGE_LENGTH = 1*1024*1024*1024  # 1GB
 
 class ClientConnections(object):
     """"Clients build connections to the cloud aggregator."""
 
-    def __init__(self, aggregator_address:str="", base_port:int=18888):
+    def __init__(self, aggregator_address:str="", base_port:int=18888,
+                 cm_ip:str="", cm_port:int=60000):
         self.base_port = base_port
         self.aggregator_address = aggregator_address
         self.channel = None
         self.stub = None
+
+        # propius
+        self.cm_ip = cm_ip
+        self.cm_port = cm_port
+        self.cm_channel = None
+        self.cm_stub = None
+
+    def connect_to_cm(self):
+        self.cm_channel = grpc.insecure_channel(f'{self.cm_ip}:{self.cm_port}')
+        self.cm_stub = propius_pb2_grpc.Client_managerStub(self.cm_channel)
+        print(f"Client new: connecting to client manager at {self.cm_ip}:{self.cm_port}")
     
     def connect_to_server(self):
         logging.info('%%%%%%%%%% Opening grpc connection to ' +
@@ -27,7 +41,7 @@ class ClientConnections(object):
         )
         self.stub = job_api_pb2_grpc.JobServiceStub(self.channel)
 
-    def close_sever_connection(self):
+    def close_server_connection(self):
         logging.info(
             '%%%%%%%%%% Closing grpc connection to the aggregator %%%%%%%%%%')
         self.channel.close()
