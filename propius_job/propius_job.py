fd993a85208ba042646d2a9cddd7292bdc630653
Eric Ding <ericdingg16@outlook.com>
2023-08-07 13:25:28 -0400

    Finish job interface

--- propius_job/propius_job.py
+++ propius_job/propius_job.py
@@ -31,8 +31,8 @@ class Propius_job():
             self.demand = job_config['demand']
             self.jm_ip = job_config['job_manager_ip']
             self.jm_port = job_config['job_manager_port']
-            self.jm_channel = None
-            self.jm_stub = None
+            self._jm_channel = None
+            self._jm_stub = None
             self.ip = job_config['ip']
             self.port = job_config['port']
 
@@ -45,7 +45,7 @@ class Propius_job():
                 self._connect_jm(self.jm_ip, self.jm_port)
                 return
             except:
-                time.sleep(1)
+                time.sleep(2)
         raise RuntimeError("Unable to connect to job manager")
     
     def _cleanup_routine(self):
@@ -61,8 +61,8 @@ class Propius_job():
             pass
         
     def _connect_jm(self, jm_ip: str, jm_port: int) -> None:
-        self.jm_channel = grpc.insecure_channel(f'{jm_ip}:{jm_port}')
-        self.jm_stub = propius_pb2_grpc.Job_managerStub(self.jm_channel)
+        self._jm_channel = grpc.insecure_channel(f'{jm_ip}:{jm_port}')
+        self._jm_stub = propius_pb2_grpc.Job_managerStub(self.jm_channel)
         # print(f"Job {self.id}: connecting to job manager at {jm_ip}:{jm_port}")   
 
     def register(self) -> bool:
@@ -77,7 +77,7 @@ class Propius_job():
 
         for _ in range(10):
             try:
-                ack_msg = self.jm_stub.JOB_REGIST(job_info_msg)
+                ack_msg = self._jm_stub.JOB_REGIST(job_info_msg)
                 self.id = ack_msg.id
                 ack = ack_msg.ack
                 if not ack:
@@ -87,6 +87,61 @@ class Propius_job():
                     # print(f"Job {self.id}: register success")
                     return True
             except:
-                pass
+                time.sleep(2)
 
         raise RuntimeError("Unable to register to Propius job manager")
+    
+    def round_start_request(self) -> bool:
+        request_msg = propius_pb2.job_round_info(
+            id=self.id,
+            demand=self.demand,
+        )
+        
+        for _ in range(10):
+            try:
+                ack_msg = self._jm_stub.JOB_REQUEST(request_msg)
+                if not ack_msg.ack:
+                    # print(
+                    #     f"Job {self.id}: round: {self.cur_round}/{self.est_total_round} request failed")
+                    return False
+                else:
+                    # print(
+                    #     f"Job {self.id}: round: {self.cur_round}/{self.est_total_round} request success")
+                    return True
+            except:
+                time.sleep(2)
+
+        raise RuntimeError("Unable to send round start request to Propius job manager at the moment")
+    
+    def round_end_request(self) -> bool:
+        """optional, terminating request"""
+        request_msg = propius_pb2.job_id(id=self.id)
+
+        for _ in range(10):
+            try:
+                ack_msg = self._jm_stub.JOB_END_REQUEST(request_msg)
+                if not ack_msg.ack:
+                    # print(
+                    #     f"Job {self.id}: round: {self.cur_round}/{self.est_total_round} end request failed")
+                    return False
+                else:
+                    # print(
+                    #     f"Job {self.id}: round: {self.cur_round}/{self.est_total_round} end request")
+                    return True
+            except:
+                    time.sleep(2)
+        
+        raise RuntimeError("Unable to send round end request to Propius job manager at this moment")
+    
+    def complete_job(self):
+        req_msg = propius_pb2.job_id(id=self.id)
+        
+        for _ in range(10):
+            try:
+                self._jm_stub.JOB_FINISH(req_msg)
+                return
+            
+            except:
+                time.sleep(2)
+
+        raise RuntimeError("Unable to send complete job request to Propius job manager at this moment")
