8b67a004531ac18fc0dc1653d2b727da21c76e09
Eric Ding <ericdingg16@outlook.com>
2023-08-07 14:11:07 -0400

    Edit comment

--- propius_job/propius_job.py
+++ propius_job/propius_job.py
@@ -13,18 +13,34 @@ def gen_job_config(public_constraint: list,
                    ps_ip: str,
                    ps_port: int
                    )->dict:
+    #TODO
     pass
 
 def encode_constraints(**kargs)->list:
     # os: 12.4 -> os: 66
     # cpu: None -> cpu: 0
     # return [66, 0]
+    #TODO 
     pass
 
 class Propius_job():
     def __init__(self, job_config):
+        """Init Propius_job class
+
+        Args:
+            job_config:
+                public_constraint
+                private_constraint
+                total_round
+                demand
+                job_manager_ip
+                job_manager_port
+                ip
+                port
+        """
         self.id = -1
         try:
+            #TODO arguments check
             self.public_constraint = tuple(job_config['public_constraint'])
             self.private_constraint = tuple(job_config['private_constraint'])
             self.est_total_round = job_config['total_round']
@@ -39,15 +55,6 @@ class Propius_job():
         except:
             raise ValueError("Missing config arguments")
         
-        # Connecting to Propius job manager server
-        for _ in range(10):
-            try:
-                self._connect_jm(self.jm_ip, self.jm_port)
-                return
-            except:
-                time.sleep(2)
-        raise RuntimeError("Unable to connect to job manager")
-    
     def _cleanup_routine(self):
         try:
             self.jm_channel.close()
@@ -55,17 +62,42 @@ class Propius_job():
             pass
     
     def __del__(self):
-        try:
-            self._cleanup_routine()
-        except:
-            pass
+        self._cleanup_routine()
+        
         
     def _connect_jm(self, jm_ip: str, jm_port: int) -> None:
         self._jm_channel = grpc.insecure_channel(f'{jm_ip}:{jm_port}')
         self._jm_stub = propius_pb2_grpc.Job_managerStub(self.jm_channel)
-        # print(f"Job {self.id}: connecting to job manager at {jm_ip}:{jm_port}")   
+        # print(f"Job {self.id}: connecting to job manager at {jm_ip}:{jm_port}") 
+
+    def connect(self) -> None:
+        """Connect to Propius job manager
+
+        Raise:
+            RuntimeError: if can't establish connection after multiple trial
+        """
+        for _ in range(10):
+            try:
+                self._connect_jm(self.jm_ip, self.jm_port)
+                return
+            except:
+                time.sleep(2)
+
+        raise RuntimeError("Unable to connect to Propius job manager at the moment")
+
+    def close(self) -> None:
+        """Clean up allocation, close connection to Propius job manager
+        """
+        self._cleanup_routine()
 
     def register(self) -> bool:
+        """Register job. Send job config to Propius job manager. This configuration will expire
+        in one week, which means the job completion time should be within one week.
+
+        Raise:
+            RuntimeError: if can't send register request after multiple trial
+        """
+
         job_info_msg = propius_pb2.job_info(
             est_demand=self.demand,
             est_total_round=self.est_total_round,
@@ -89,12 +121,36 @@ class Propius_job():
             except:
                 time.sleep(2)
 
-        raise RuntimeError("Unable to register to Propius job manager")
+        raise RuntimeError("Unable to register to Propius job manager at the moment")
     
-    def round_start_request(self) -> bool:
+    def round_start_request(self, new_demand: bool, demand:int=0) -> bool:
+        """Send round start request to Propius job manager. Client will be routed to parameter server after this call
+        until the number of clients has reached specified demand, or round_end_request is called. 
+        Note that though Propius provide the guarantee that the requested demand will be satisfied, 
+        allocated clients may experience various issues such as network failure
+        such that the number of check-in clients might be lower than what is demanded at the parameter server
+        
+        Args:
+            new_demand: boolean indicating whether to use a new demand number for this round (only)
+            demand: positive integer indicating number of demand in this round. 
+                    If not specified, will use the default demand which is specified in the initial configuration
+
+        Raise:
+            RuntimeError: if can't send request after multiple trial
+            ValueError: if input demand is not a positive integer
+        """
+
+        if not new_demand:
+            this_round_demand = self.demand
+        else:
+            if demand <= 0:
+                raise ValueError("Input demand number is not a positive integer")
+            else:
+                this_round_demand = demand
+
         request_msg = propius_pb2.job_round_info(
             id=self.id,
-            demand=self.demand,
+            demand=this_round_demand
         )
         
         for _ in range(10):
@@ -114,7 +170,13 @@ class Propius_job():
         raise RuntimeError("Unable to send round start request to Propius job manager at the moment")
     
     def round_end_request(self) -> bool:
-        """optional, terminating request"""
+        """Send round end request to Propius job manager. Client won't be routed to parameter server after this call, 
+        unless round_start_request is called
+
+        Raise:
+            RuntimeError: if can't send round end request after multiple trial
+        """
+
         request_msg = propius_pb2.job_id(id=self.id)
 
         for _ in range(10):
@@ -134,6 +196,12 @@ class Propius_job():
         raise RuntimeError("Unable to send round end request to Propius job manager at this moment")
     
     def complete_job(self):
+        """Send complete job request to Propius job manager. Job configuration will be removed from Propius.
+        
+        Raise:
+            RuntimeError: if can't send complete_job request after multiple trial
+        """
+
         req_msg = propius_pb2.job_id(id=self.id)
         
         for _ in range(10):
