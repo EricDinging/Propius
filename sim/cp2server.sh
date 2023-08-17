209410f9c92fee79a6e06c3ac1aad8fb1dca19a1
EricDinging <ericdingg16@outlook.com>
2023-08-16 22:38:31 -0400

    Start new round

--- sim/cp2server.sh
+++ sim/cp2server.sh
@@ -1,12 +1,16 @@
-if [ $# -eq 0 ]; then
-  echo "No argument provided."
-  exit 1
-fi
+# if [ $# -eq 0 ]; then
+#   echo "No argument provided."
+#   exit 1
+# fi
 
-if [ $1 = 1 ]; then
-    scp -r ./amg EricD16@clnode238.clemson.cloudlab.us:~/
-elif [ $1 = 2 ]; then
-    scp -r ./amg EricD16@clnode226.clemson.cloudlab.us:~/
-elif [ $1 = 3 ]; then
-    scp -r ./amg EricD16@clnode244.clemson.cloudlab.us:~/
-fi
+# if [ $1 = 1 ]; then
+#     scp -r ./amg EricD16@clnode238.clemson.cloudlab.us:~/
+# elif [ $1 = 2 ]; then
+#     scp -r ./amg EricD16@clnode226.clemson.cloudlab.us:~/
+# elif [ $1 = 3 ]; then
+#     scp -r ./amg EricD16@clnode244.clemson.cloudlab.us:~/
+# fi
+
+scp -r ./evaluation EricD16@clgpu011.clemson.cloudlab.us:~/Propius/
+scp -r ./propius EricD16@clgpu011.clemson.cloudlab.us:~/Propius/
+scp -r ./propius_job EricD16@clgpu011.clemson.cloudlab.us:~/Propius/
