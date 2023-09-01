4f2a3543fb3ce320fdb4cde64851a5553e7252ae
EricDinging <ericdingg16@outlook.com>
2023-08-31 22:22:26 -0400

    Add clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,10 +1,5 @@
-rm -f ./evaluation/single_executor/result*/* 
-rm ./evaluation/job/ps_result*/*
-rm ./evaluation/executor/result*/*
-rm ./evaluation/client/*.log*
-rm ./evaluation/job/*.log*
-rm ./evaluation/executor/*.log*
-rm ./propius/client_manager/*.log*
-rm ./propius/load_balancer/*.log*
-rm ./propius/job_manager/*.log*
-rm ./propius/scheduler/*.log*
+rm ./propius/monitor/log/*
+rm ./propius/monitor/plot/*
+rm ./evaluation/monitor/client/*
+rm ./evaluation/monitor/executor/*
+rm ./evaluation/monitor/job/*
