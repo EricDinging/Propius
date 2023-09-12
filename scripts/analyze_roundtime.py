f4cd0aa51237d9961a1aa28708c2f0b6dd682ff3
EricDinging <ericdingg16@outlook.com>
2023-09-12 11:30:26 -0400

    Edit analyze py

--- scripts/analyze_roundtime.py
+++ scripts/analyze_roundtime.py
@@ -23,37 +23,67 @@ def read_last_line(csv_file):
                 sched_time = float(row[2])
                 resp_time = float(row[3])
             last_row = row
+
+def read_first(round, csv_file):
+    with open(csv_file, 'r', newline='') as file:
+        csv_reader = csv.reader(file)
+        time_round = 0
+        sched_time = 0
+        resp_time = 0
+        num = 0
+        for row in csv_reader:
+            if row[0] == "round":
+                continue
+            sched_time += float(row[2])
+            resp_time += float(row[3])
+            num += 1
+            if num == round:
+                time_round = float(row[1])
+                break
+
+        sched_time /= num if num > 0 else 1
+        resp_time /= num if num > 0 else 1
     
+        return (time_round, sched_time, resp_time)
 
-        return (round_time, sched_time, resp_time)
-upper_round = upper_sched = upper_resp =  0
-total_round = total_sched = total_resp = 0
-lower_round = lower_sched = lower_resp = 1000000000
-num = 0  
-for filename in os.listdir(folder_path):
-    
-    if filename.endswith(".csv"):
-        csv_file_path = os.path.join(folder_path, filename)
-        # print(csv_file_path)
-        round_time, sched, response = read_last_line(csv_file_path)
+analyze_certain_rounds = input("Analyze certain rounds y/n: ") == 'y'
 
-        num += 1 
-        total_round += round_time
-        total_sched += sched
-        total_resp += response
+if analyze_certain_rounds:
+    round = int(input("round: "))
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".csv"):
+            csv_file_path = os.path.join(folder_path, filename)
+            round_time, sched, response = read_first(round, csv_file_path)
+            print(f"{filename}, round: {round} time: {round_time}, avg sched: {sched}, avg_response: {response}")
+else:
+    upper_round = upper_sched = upper_resp =  0
+    total_round = total_sched = total_resp = 0
+    lower_round = lower_sched = lower_resp = 1000000000
+    num = 0  
+    for filename in os.listdir(folder_path):
+        
+        if filename.endswith(".csv"):
+            csv_file_path = os.path.join(folder_path, filename)
+            # print(csv_file_path)
+            round_time, sched, response = read_last_line(csv_file_path)
 
-        upper_round = round_time if round_time > upper_round else upper_round
-        lower_round = round_time if round_time < lower_round else lower_round
-        upper_sched = sched if sched > upper_sched else upper_sched
-        lower_sched = sched if sched < lower_sched else lower_sched
-        upper_resp = response if response > upper_resp else upper_resp
-        lower_resp = response if response < lower_resp else lower_resp
+            num += 1 
+            total_round += round_time
+            total_sched += sched
+            total_resp += response
 
-avg_round = total_round / num
-avg_sched = total_sched / num
-avg_response = total_resp / num
-print(f"Avg finish time: {avg_round}, avg sched delay: {avg_sched}, avg response time: {avg_response}")
-print(f"Upper finish time: {upper_round}, Lower finish time: {lower_round}")
-print(f"Upper sched time: {upper_sched}, Lower sched time: {lower_sched}")
-print(f"Upper response time: {upper_resp}, Lower response time: {lower_resp}")
+            upper_round = round_time if round_time > upper_round else upper_round
+            lower_round = round_time if round_time < lower_round else lower_round
+            upper_sched = sched if sched > upper_sched else upper_sched
+            lower_sched = sched if sched < lower_sched else lower_sched
+            upper_resp = response if response > upper_resp else upper_resp
+            lower_resp = response if response < lower_resp else lower_resp
+
+    avg_round = total_round / num
+    avg_sched = total_sched / num
+    avg_response = total_resp / num
+    print(f"Avg finish time: {avg_round}, avg sched delay: {avg_sched}, avg response time: {avg_response}")
+    print(f"Upper finish time: {upper_round}, Lower finish time: {lower_round}")
+    print(f"Upper sched time: {upper_sched}, Lower sched time: {lower_sched}")
+    print(f"Upper response time: {upper_resp}, Lower response time: {lower_resp}")
 
