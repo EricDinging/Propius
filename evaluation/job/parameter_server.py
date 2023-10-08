8b10c141ef255387813f4572f12ebd53214a3604
EricDinging <ericdingg16@outlook.com>
2023-10-08 11:53:37 -0400

    Start test

--- evaluation/job/parameter_server.py
+++ evaluation/job/parameter_server.py
@@ -512,7 +512,7 @@ if __name__ == '__main__':
                 config['sched_alg'] = eval_config['sched_alg']
                 config['do_compute'] = eval_config['do_compute']
                 config['speedup_factor'] = eval_config['speedup_factor']
-                config['model_size'] = 10
+                # config['model_size'] = 10
                 loop = asyncio.get_event_loop()
                 loop.run_until_complete(run(config))
                 
