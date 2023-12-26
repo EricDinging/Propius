09e92d90b5ee1ea9c61261c58be74884ec117069
EricDinging <ericdingg16@outlook.com>
2023-12-25 23:47:57 -0500

    Add register test case

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="propius",
-    version="0.5",
-    description="An efficient FL resource manager",
-    url="https://github.com/EricDinging/Propius",
-    packages=find_packages(),
-)
