1e1d8c43220c7dcf2b42f7de52febf05b895b5b9
EricDinging <ericdingg16@outlook.com>
2023-12-27 12:05:32 -0500

    Refactor offline scheduler

--- propius_controller/scheduler/offline_module/base_scheduler.py
+++ propius_controller/scheduler/offline_module/base_scheduler.py
@@ -1,15 +1,13 @@
-"""Job base scheduler class."""
-
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from propius_controller.util import Msg_level, Propius_logger
 from propius_controller.scheduler.sc_monitor import SC_monitor
 from propius_controller.scheduler.sc_db_portal import (
     SC_client_db_portal,
     SC_job_db_portal,
 )
-from propius_controller.scheduler.sc_job_group import SC_job_group_manager
 from propius_controller.channels import propius_pb2_grpc
 from propius_controller.channels import propius_pb2
+from propius_controller.util.commons import Job_group
 import pickle
 import asyncio
 import time
@@ -44,18 +42,11 @@ class Scheduler(propius_pb2_grpc.SchedulerServicer):
         self.ip = gconfig["scheduler_ip"] if not gconfig["use_docker"] else "0.0.0.0"
         self.port = gconfig["scheduler_port"]
 
-        # TODO move to main
-        # self.sched_alg = gconfig["sched_alg"]
-        # if self.sched_alg == "irs":
-        #     self.irs_epsilon = float(gconfig["irs_epsilon"])
-
         self.job_db_portal = SC_job_db_portal(gconfig, logger)
         self.client_db_portal = SC_client_db_portal(gconfig, logger)
 
         self.public_max = gconfig["public_max"]
 
-        # self.std_round_time = gconfig["standard_round_time"]
-        # self.constraints = []
         self.public_constraint_name = gconfig["job_public_constraint"]
 
         self.sc_monitor = SC_monitor(
@@ -65,73 +56,39 @@ class Scheduler(propius_pb2_grpc.SchedulerServicer):
 
         self.start_time = time.time()
 
-        # self.job_group_manager = SC_job_group_manager(
-        #     self.job_db_portal,
-        #     self.client_db_portal,
-        #     self.public_constraint_name,
-        #     self.public_max,
-        #     logger,
-        # )
-
-    @abstractmethod
-    async def online(self):
-        pass
+        self.job_group = Job_group()
+        self.lock = asyncio.Lock()
 
     @abstractmethod
     async def offline(self):
         pass
 
+    @abstractmethod
+    async def new_job(self, job_id: int):
+        pass
+
+    async def schedule_routine(self):
+        while True:
+            async with self.lock:
+                await self.offline()
+            asyncio.sleep(5)
+
+    async def GET_JOB_GROUP(self, request, context):
+        async with self.lock:
+            return propius_pb2.group_info(group=pickle.dumps(self.job_group))
+
     async def JOB_SCORE_UPDATE(self, request, context) -> propius_pb2.ack:
-        """Service function that update scores of job in database
+        """Service function that update metadata of job in database for offline scheduler
 
         Args:
             request: job manager request message: job_id.id
             context:
         """
         job_id = request.id
-
-        await self.sc_monitor.request(job_id)
-
-        # job_size = self.job_db_portal.get_job_size()
-
-        self.logger.print("Receive update score request", Msg_level.INFO)
-
-        await self.online(job_id)
-
-        # if self.sched_alg == "irs":
-        #     # Update every job score using IRS
-        #     await self._irs_score(job_id)
-        # elif self.sched_alg == "irs2":
-        #     # Update every job socre using IRS with a slight tweek that has experimental
-        #     # performance improvement
-        #     await self._irs2_score(job_id)
-
-        # elif self.sched_alg == "irs3":
-        #     # Update every job score using IRS
-        #     self.job_group_manager.update_job_group(job_id != -1, job_id)
-
-        # elif self.sched_alg == "srsf":
-        #     # Give every job a score of -remaining demand.
-        #     # remaining demand = remaining round * current round demand
-        #     # Prioritize job with the smallest remaining demand
-        #     self.job_db_portal.srsf_update_all_job_score()
-
-        # elif self.sched_alg == "srtf":
-        #     # Give every job a score of -remaining time
-        #     # remaining time = past avg round time * remaining round
-        #     # Prioritize job with the shortest remaining demand
-        #     self.job_db_portal.srtf_update_all_job_score(self.std_round_time)
-
-        # elif self.sched_alg == "las":
-        #     # Give every job a score of -attained service
-        #     self.job_db_portal.las_update_all_job_score()
-
+        async with self.lock:
+            self.new_job(job_id)
+            
         return propius_pb2.ack(ack=True)
-
-    async def GET_JOB_GROUP(self, request, context):
-        return propius_pb2.group_info(
-            group=pickle.dumps(self.job_group_manager.fetch_job_group())
-        )
 
     async def HEART_BEAT(self, request, context):
         return propius_pb2.ack(ack=True)
