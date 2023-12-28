1793f8d14d5e33ee5a60e752f152998cc5e3d823
EricDinging <ericdingg16@outlook.com>
2023-12-28 12:08:23 -0500

    Refactor IRS

--- propius_controller/scheduler/offline_module/irs_scheduler.py
+++ propius_controller/scheduler/offline_module/irs_scheduler.py
@@ -15,9 +15,8 @@ class IRS_scheduler(Scheduler):
         if not constraints:
             return False
         # Insert cst to job group
-        self.job_group.insert_cst(constraints)
-        self.logger.print(f"Insert new constraint group: {constraints}", Msg_level.INFO)
-        return propius_pb2.ack(ack=True)
+        self.job_group.insert_key(constraints)
+        self.logger.print(f"insert new constraint group: {constraints}", Msg_level.INFO)
 
     async def job_request(self, job_id: int):
         pass
@@ -31,29 +30,29 @@ class IRS_scheduler(Scheduler):
             # Clear past job group info
             self.job_group.clear_group_info()
 
-            for cst in self.job_group.constraint_list:
+            for cst in self.job_group.key_list:
                 # iterate over constraint, get updated and sorted job list
                 if not self.job_db_portal.get_job_list(
-                    cst, self.job_group.cst_job_group_map[cst]
+                    cst, self.job_group.key_job_group_map[cst]
                 ):
-                    self.job_group.remove_cst(cst)
+                    self.job_group.remove_key(cst)
 
-                for job_id in self.job_group.cst_job_group_map[cst]:
+                for job_id in self.job_group.key_job_group_map[cst]:
                     sched, resp = self.job_db_portal.get_sched_resp(job_id)
                     job_time_ratio_map[job_id] = resp / sched if sched > 0 else 1
 
-            self.logger.print(f"Finding eligible client size", Msg_level.INFO)
+            self.logger.print(f"finding eligible client size", Msg_level.INFO)
             # search elig client size for each group
-            for cst in self.job_group.constraint_list:
+            for cst in self.job_group.key_list:
                 constraints_client_map[
                     cst
                 ] = self.client_db_portal.get_client_proportion(cst)
 
             # Update group query 1
             client_size = self.client_db_portal.get_client_size()
-            self.job_group.constraint_list.sort(key=lambda x: constraints_client_map[x])
+            self.job_group.key_list.sort(key=lambda x: constraints_client_map[x])
             bq = ""
-            for cst in self.job_group.constraint_list:
+            for cst in self.job_group.key_list:
                 this_q = ""
                 for idx, name in enumerate(self.public_constraint_name):
                     this_q += f"@{name}: [{cst[idx]}, {self.public_max[name]}] "
@@ -68,18 +67,18 @@ class IRS_scheduler(Scheduler):
                 bq = bq + f" -({this_q})"
 
             # Update group query 2
-            self.job_group.constraint_list.sort(
+            self.job_group.key_list.sort(
                 key=lambda x: constraints_client_map[x], reverse=True
             )
-            for idx, cst in enumerate(self.job_group.constraint_list):
-                for h_cst in self.job_group.constraint_list[idx + 1 :]:
+            for idx, cst in enumerate(self.job_group.key_list):
+                for h_cst in self.job_group.key_list[idx + 1 :]:
                     m = self.job_db_portal.get_affected_len(
-                        self.job_group.cst_job_group_map[cst],
-                        self.job_group.cst_job_group_map[h_cst],
+                        self.job_group.key_job_group_map[cst],
+                        self.job_group.key_job_group_map[h_cst],
                         constraints_client_map[cst],
                         constraints_client_map[h_cst],
                     )
-                    m_h = len(self.job_group.cst_job_group_map[h_cst])
+                    m_h = len(self.job_group.key_job_group_map[h_cst])
                     if (
                         m / constraints_alloc_map[cst]
                         > m_h / constraints_alloc_map[h_cst]
