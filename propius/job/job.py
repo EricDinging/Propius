b43a09c2cbfee4f12ea62660c18eef238f503d88
EricDinging <ericdingg16@outlook.com>
2024-01-07 21:45:35 -0500

    Add comments

--- propius/job/job.py
+++ propius/job/job.py
@@ -5,6 +5,27 @@ import time
 
 class Job:
     def __init__(self, config: dict, verbose: bool = False, logging: bool = False):
+        """Init Propius_job class
+
+        Args:
+            job_config:
+                public_constraint: dict
+                private_constraint: dict
+                total_round: optional
+                demand: estimated demand per round
+                job_manager_ip
+                job_manager_port
+                ip: ip of the job server
+                port: port number of the job server
+                root_ps_ip
+                root_ps_port
+            verbose: whether to print or not
+            logging: whether to log or not
+
+        Raises:
+            ValueError: missing config args
+        """
+
         self.job_controller = Propius_job(config, verbose, logging)
 
         self.id = -1
@@ -13,12 +34,20 @@ class Job:
         self.verbose = verbose
         self.logging = logging
 
-        self.total_round = config["total_round"]
+        self.total_round = config["total_round"] if "total_round" in config else -1
         self.demand = config["demand"]
 
         self.round = 0
 
     def register(self) -> bool:
+        """Register job. Send job config to Propius job manager.
+
+        Returns:
+            a boolean indicating the status of registration
+
+        Raise:
+            RuntimeError: if can't send register request after multiple trial
+        """
         if self.job_controller.register():
             self.id = self.job_controller.id
 
@@ -30,6 +59,23 @@ class Job:
             return False
 
     def request(self, meta: dict, data: list, demand: int = -1) -> bool:
+        """Send round request to Propius.
+        If the controller accepts the request, round metadata and parameter data are sent to the paremeter server for client retrieval.
+
+        Args:
+            meta: a dictionary of configuration
+            data: a list of tensor
+            demand: optional. The round demand for this round will change if specified
+
+        Returns:
+            a boolean indicating whether the call was successful.
+            If return False, it might be due to the request time exceeds the
+            total round configuration during initalization.
+
+        Raise:
+            RuntimeError: if can't send request after multiple trial
+        """
+
         new_demand = demand > 0
         this_round_demand = self.demand if not new_demand else demand
 
@@ -41,19 +87,32 @@ class Job:
         return False
 
     def reduce(self, timeout: float = 60):
+        """Get round result from parameter server, as well as terminate round request at controller.
+
+        This is a blocking call. It can be called multiple times if the previous calls are not successful. Another request call is needed after a successful reduce call.
+
+        Args:
+            timeout: default to 60 seconds
+
+        Returns:
+            (metadata, data) if successful (round demand satisfied), else None
+
+        Raises:
+            RuntimeError: if can't send end request after multiple trial or internal parameter server error
+        """
         if self.round == -1:
             return None
-        
+
         start_time = time.time()
         while True:
             code, meta, data = self.job_ps.get(self.round)
             if code == 1:
                 self.job_controller.end_request()
-
+                self.round = -1
                 return (meta, data)
 
             elif code == 3:
-                raise RuntimeError("error code 3 from parameter server")
+                raise RuntimeError("error from parameter server")
 
             if time.time() - start_time >= timeout:
                 break
@@ -62,5 +121,10 @@ class Job:
         return None
 
     def complete(self):
+        """Job completion call. Job metadata and parameter entry will be deleted after this call.
+
+        Raise:
+            RuntimeError: if can't send request after multiple trial
+        """
         self.job_controller.complete_job()
         self.job_ps.delete()
